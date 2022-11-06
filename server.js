4da93e26326aa7b5ff2f22ea706fc40f83641e6b
Pepe Amoedo <pepeamoedo@gmail.com>
2022-11-06 19:05:11 +0100

    Confirmado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => res.send("<h1>Hey Mac-key!</h1>"))
+app.get('/', (req,res) => res.send("<h1>Hey!</h1>"))
 
-app.listen(8080);
+app.listen(80);
 console.log('Server on port 8080');
